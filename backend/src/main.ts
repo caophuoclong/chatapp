5781f68024639f48847c3a6ef14ef1093b5551b3
caophuoclong <caophuoclong1@gmail.com>
2022-09-17 23:55:40 +0700

    update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,10 +3,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {SwaggerModule, DocumentBuilder} from "@nestjs/swagger"
 import cookieParser  from "cookie-parser";
+import * as dotenv from 'dotenv';
+dotenv.config()
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ["http://192.168.193.194:3000", "http://localhost:3000"],
+    origin: ["http://192.168.193.194:3000", "http://localhost:3000", "https://6325fb1041b1717377238ccc--sparkling-gaufre-774277.netlify.app", "https://bebes.site"],
     credentials: true
   });
   const config = new DocumentBuilder()
@@ -20,6 +22,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe())
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3003);
+  await app.listen(process.env.PORT || 3003);
 }
 bootstrap();
